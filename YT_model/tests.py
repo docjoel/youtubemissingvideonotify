94220c04f49978bb585a2633b77f825f87ef41ce
docjoel <joelschooler@me.com>
2018-03-07 11:38:08 -0800

    moved order

--- YT_model/tests.py
+++ YT_model/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from .models import *
+from .models import You_TubeChannel
 # Create your tests here.
+
 class ChannelModelTest(TestCase):
 
 
     channel = You_TubeChannel("UUVykYhKkOLuIKVr7F0b1npg", "Unmedicated and Dysfunctional", 297, 0)
-
+    channel.save()
 
     def test_build(self):
 
