51d7a31dd7ecb174f0c9777e11f35cf2d7c79cb3
docjoel <joelschooler@me.com>
2018-03-07 09:59:28 -0800

    adding in postgres support

--- YT_model/tests.py
+++ YT_model/tests.py
@@ -4,5 +4,12 @@ from .models import *
 class ChannelModelTest(TestCase):
 
 
-    def setUp(self):
-       channel = You_TubeChannel("UUVykYhKkOLuIKVr7F0b1npg", "Unmedicated and Dysfunctional", 297, 0)
+
+    channel = You_TubeChannel("UUVykYhKkOLuIKVr7F0b1npg", "Unmedicated and Dysfunctional", 297, 0)
+
+
+    def test_build(self):
+
+        self.assertIs(self.channel.channelName == "Unmedicated and Dysfunctional")
+
+
