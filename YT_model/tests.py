8823945bd8c026353e00755c1ac54e79b2a237c1
docjoel <joelschooler@me.com>
2018-03-07 12:10:50 -0800

    moved order

--- YT_model/tests.py
+++ YT_model/tests.py
@@ -11,18 +11,20 @@ class ChannelModelTest(TestCase):
     def test_build(self):
 
         self.assertEqual(self.channel.channelName, "Unmedicated and Dysfunctional")
-
+        print("Passed Build")
 
     def test_addVideo(self):
         self.assertEqual(self.channel.number_of_videos,297)
         self.channel.update_video_count(298)
         self.assertEqual(self.channel.number_of_videos,298)
         self.assertEqual(self.channel.number_of_old_videos,297)
+        print("passed added video")
 
 
     def test_missing(self):
         self.channel.update_video_count(280)
-        self.assertEqual(self.channel.video_missing,True)
+        self.assertEqual(self.channel.video_missing(),True)
+        print("passed missing")
 
 
 
