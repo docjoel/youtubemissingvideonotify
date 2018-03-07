e3e2d362010ea99328ca7e028e8f3672f3bd0a12
docjoel <joelschooler@me.com>
2018-03-07 12:18:28 -0800

    moved order

--- YT_model/models.py
+++ YT_model/models.py
@@ -22,8 +22,8 @@ class You_TubeChannel(models.Model):
                 "Number of Videos":You_TubeChannel.number_of_videos}
     def video_missing(self):
         #returns True if the number of old videos is greater than the current number of videos
-        print(You_TubeChannel.number_of_videos)
-        return (You_TubeChannel.number_of_old_videos > You_TubeChannel.number_of_videos)
+        return (self.number_of_old_videos > self.number_of_videos)
+
     def update_video_count(self,new_video_count):
         self.number_of_old_videos = self.number_of_videos
         self.number_of_videos = new_video_count
