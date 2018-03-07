974be04a30f7cfe6cb7f621e31d9f0211b35f6a1
docjoel <joelschooler@me.com>
2018-03-07 09:30:27 -0800

    adding in postgres support

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 apk update
-apk add libpq-dev python3-dev
-apk add postgresql-server-dev-all
 apk add py3-psycopg2
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
-python3 manage.py makemigrations YT_model
+python3 manage.py makemigrations YT_models
 python3 manage.py migrate
 python3 manage.py test channel_models
